195ddeb9c3434b7df41e2a46c514477258f857ee
BlackScythe0178 <shadowwarhawk84@gmail.com>
2026-08-31 07:41:44 -0400

    Implement pagination for hot collections display
    Added pagination functionality to HotCollections component.

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 
 const HotCollections = () => {
   const [collections, setCollections] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const itemsPerPage = 4;
 
   useEffect(() => {
     const fetchHotCollections = async () => {
@@ -18,7 +21,6 @@ const HotCollections = () => {
         }
 
         const data = await response.json();
-
         setCollections(data);
       } catch (error) {
         console.error(error);
@@ -30,6 +32,22 @@ const HotCollections = () => {
 
     fetchHotCollections();
   }, []);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex + itemsPerPage >= collections.length
+        ? 0
+        : prevIndex + itemsPerPage
+    );
+  };
+
+  const previousSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex - itemsPerPage < 0
+        ? Math.floor((collections.length - 1) / itemsPerPage) * itemsPerPage
+        : prevIndex - itemsPerPage
+    );
+  };
 
   if (loading) {
     return (
@@ -74,50 +92,72 @@ const HotCollections = () => {
             </div>
           </div>
 
-          {collections.map((collection, index) => (
-            <div
-              className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              key={collection.id}
-              data-aos="fade-up"
-              data-aos-duration="650"
-              data-aos-delay={(index % 4) * 100}
-              data-aos-offset="100"
-              data-aos-once="true"
-            >
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to={`/item-details/${collection.nftId}`}>
-                    <img
-                      src={collection.nftImage}
-                      className="lazy img-fluid"
-                      alt={collection.title}
-                    />
-                  </Link>
-                </div>
+          {collections
+            .slice(currentIndex, currentIndex + itemsPerPage)
+            .map((collection, index) => (
+              <div
+                className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                key={collection.id}
+                data-aos="fade-up"
+                data-aos-duration="650"
+                data-aos-delay={(index % 4) * 100}
+                data-aos-offset="100"
+                data-aos-once="true"
+              >
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to={`/item-details/${collection.nftId}`}>
+                      <img
+                        src={collection.nftImage}
+                        className="lazy img-fluid"
+                        alt={collection.title}
+                      />
+                    </Link>
+                  </div>
 
-                <div className="nft_coll_pp">
-                  <Link to={`/author/${collection.authorId}`}>
-                    <img
-                      className="lazy pp-coll"
-                      src={collection.authorImage}
-                      alt={`${collection.title} author`}
-                    />
-                  </Link>
+                  <div className="nft_coll_pp">
+                    <Link to={`/author/${collection.authorId}`}>
+                      <img
+                        className="lazy pp-coll"
+                        src={collection.authorImage}
+                        alt={`${collection.title} author`}
+                      />
+                    </Link>
 
-                  <i className="fa fa-check"></i>
-                </div>
+                    <i className="fa fa-check"></i>
+                  </div>
 
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{collection.title}</h4>
-                  </Link>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{collection.title}</h4>
+                    </Link>
 
-                  <span>ERC-{collection.code}</span>
+                    <span>ERC-{collection.code}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
+
+        {collections.length > itemsPerPage && (
+          <div className="d-flex justify-content-center gap-3 mt-4">
+            <button
+              type="button"
+              className="btn-main"
+              onClick={previousSlide}
+            >
+              Previous
+            </button>
+
+            <button
+              type="button"
+              className="btn-main"
+              onClick={nextSlide}
+            >
+              Next
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
